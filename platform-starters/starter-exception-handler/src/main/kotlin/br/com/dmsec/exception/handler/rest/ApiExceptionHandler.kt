d43f0d3b207b40d3ef23eca39abe757d8906e323
Douglas Marra <douglasmsi@gmail.com>
2021-01-11 19:18:12 -0300

    em andamento exception-handler

--- platform-starters/starter-exception-handler/src/main/kotlin/br/com/dmsec/exception/handler/rest/ApiExceptionHandler.kt
+++ platform-starters/starter-exception-handler/src/main/kotlin/br/com/dmsec/exception/handler/rest/ApiExceptionHandler.kt
@@ -1,8 +1,17 @@
 package br.com.dmsec.exception.handler.rest
 
 
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import br.com.dmsec.exception.handler.rest.error.ErrorMessageResponse
+import br.com.dmsec.exception.handler.rest.error.ErrorMessageResponseGenerator
+import br.com.dmsec.starter.commons.exception.BusinessDynamicException
+import br.com.dmsec.starter.commons.exception.BusinessException
+import br.com.dmsec.starter.commons.exception.NotFoundException
+import br.com.dmsec.starter.commons.exception.UnauthorizedException
+import br.com.dmsec.starter.commons.logger.logger
+import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
 
 
 
